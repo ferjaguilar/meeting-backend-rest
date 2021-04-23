e4b140550bb47eb08164a98394965d91b7a320ce
Fernando Aguilar <ferrivas127@gmail.com>
2021-04-22 18:28:57 -0600

    Create get-meetings route

--- src/routes/Meeting.ts
+++ src/routes/Meeting.ts
@@ -4,7 +4,7 @@ import StudentsModel from '../models/Students';
 
 const router = Router();
 
-router.post('/add-meeting', async (req:Request, res:Response) => {
+router.post('/add-meeting', async (req:Request, res:Response):Promise<Response> => {
   const { body } = req;
   try {
     const meetingDB = await MeetingModel.create(body);
@@ -12,6 +12,16 @@ router.post('/add-meeting', async (req:Request, res:Response) => {
     return res.status(201).json(meetingDB);
   } catch (error) {
     return res.status(400).json({ message: 'Error /add-meeting', error });
+  }
+});
+
+router.get('/get-meetings', async (req: Request, res:Response):Promise<Response> => {
+  const { limit = 5, from = 0 } = req.query;
+  try {
+    const meetingDB = await MeetingModel.find().populate('studentId').skip(Number(from)).limit(Number(limit));
+    return res.status(200).json(meetingDB);
+  } catch (error) {
+    return res.status(400).json({ message: 'Error /get-meetings', error });
   }
 });
 
