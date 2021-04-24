bbc119f78d6e25455d56aee1c2c0087a0136e7fd
Fernando Aguilar <ferrivas127@gmail.com>
2021-04-24 10:44:15 -0600

    Update student completed modify

--- src/routes/Meeting.ts
+++ src/routes/Meeting.ts
@@ -8,7 +8,7 @@ router.post('/add-meeting', async (req:Request, res:Response):Promise<Response> 
   const { body } = req;
   try {
     const meetingDB = await MeetingModel.create(body);
-    await StudentsModel.findByIdAndUpdate(meetingDB._id, { completed: true }, { new: true });
+    await StudentsModel.findByIdAndUpdate(body.studentId, { completed: true }, { new: true });
     return res.status(201).json(meetingDB);
   } catch (error) {
     return res.status(400).json({ message: 'Error /add-meeting', error });
